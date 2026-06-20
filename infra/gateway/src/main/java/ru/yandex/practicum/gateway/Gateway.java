bb34ab55942c891070c1d5b9434c113dc4ee1375
Stefan <stef.kir1999@gmail.com>
2026-06-20 23:45:11 +0400

    Spring Cloud Gateway

---
+++ infra/gateway/src/main/java/ru/yandex/practicum/gateway/Gateway.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Gateway {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class,args);
+    }
+}
